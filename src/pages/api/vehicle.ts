db505a4bc70b670b864b735b83b6a5f4cdd74319
Ethan Vaz <francisxvaz@gmail.com>
2020-07-02 06:39:58 +1000

    check api

---
+++ src/pages/api/vehicle.ts
@@ -0,0 +1,7 @@
+import { NextApiRequest,NextApiResponse } from "next";
+
+export default function getAllVehicles(req: NextApiRequest,res: NextApiResponse){
+
+    res.json({hello: "world", method:req.method});
+
+}
