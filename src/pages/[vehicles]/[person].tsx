aebf9d58e8fc3e4061a6ba433bc25c9424658748
Ethan Vaz <francisxvaz@gmail.com>
2020-07-01 13:55:43 +1000

    dynamic routing

---
+++ src/pages/[vehicles]/[person].tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Bruno() {
+    return (
+        <div>
+            Here is the car!!
+        </div>
+    )
+}
+
+export default Bruno
---
+++ src/pages/[vehicles]/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function index() {
+    return (
+        <div>
+            Index page for vehicles!!
+        </div>
+    )
+}
+
+export default index
